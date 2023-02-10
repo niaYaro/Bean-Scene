2dee99d33c60078b2ec912b0e1bcce39a4884167
niaYaro <yarovyi@gmail.com>
2023-02-10 16:00:10 +0200

    Blends sec. is done

---
+++ js/blends-gallery-fs.js
@@ -0,0 +1,11 @@
+new Swiper('.blends-gallery-fs', {
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 4
+})
---
+++ js/blends-gallery-mb.js
@@ -0,0 +1,11 @@
+new Swiper('.blends-gallery-mb', {
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 1
+})
