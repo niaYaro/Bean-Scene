50e91badeb6a8dca32a99ff076b5805de47d0954
niaYaro <yarovyi@gmail.com>
2023-02-15 18:00:29 +0200

    Finished

---
+++ js/feedback-gallery-fs.js
@@ -0,0 +1,8 @@
+new Swiper('.feedback-swiper-fs', {
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 1
+})
